0e76cfea4e1ec8011623013b49c9c2e52511499a
Jhon Melendez <jhonmelendez77@gmail.com>
2024-05-08 20:58:11 -0500

    Actualizacion y eliminacion de datos

--- src/Main.java
+++ src/Main.java
@@ -6,16 +6,26 @@ public class Main {
     public static void main(String[] args) {
         Connection myConn = null;
         Statement myStamt = null;
-        ResultSet MyRes = null;
+        ResultSet myRes = null;
         try{
             myConn = DriverManager.getConnection("jdbc:mysql://localhost:3306/project","root","");
             System.out.println("Conexion exitosa");
 
+        //Actualizar datos
             myStamt = myConn.createStatement();
-            MyRes = myStamt.executeQuery("SELECT * FROM employees");
+            int rowsAffected = myStamt.executeUpdate("UPDATE employees " + "set email='johanador@example.com' " + "WHERE first_name = 'Johana'");
+            myRes = myStamt.executeQuery("SELECT * FROM employees");
+            while (myRes.next()){
+                System.out.println(myRes.getString("first_name")+","+ myRes.getString("email"));
+            }
 
-            while (MyRes.next()){
-                System.out.println(MyRes.getString("first_name"));
+        //Borrar Datos
+            myStamt = myConn.createStatement();
+            int rowsAffected2 = myStamt.executeUpdate("DELETE FROM employees " + "WHERE first_name = 'Johana'");
+            System.out.println("empleados");
+            myRes = myStamt.executeQuery("SELECT * FROM employees");
+            while (myRes.next()){
+                System.out.println(myRes.getString("first_name"));
             }
 
         }catch (SQLException e){
